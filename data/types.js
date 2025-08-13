30d38c3980750152777dff73fc6166ba5012b637
TheSulfateForge <thesulfateforge@gmail.com>
2025-08-13 11:19:35 -0500

    Delete data/types.js

--- data/types.js
+++
@@ -1,7 +0,0 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// This file is intentionally left blank. 
-// It originally contained TypeScript type definitions, which are not used at runtime.
